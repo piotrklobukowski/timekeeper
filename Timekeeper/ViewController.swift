c5bf5b8810e47fd05322a585b157c0efa9cb5b73
Piotr Klobukowski <piotr.klobukowski@gmail.com>
2020-01-09 15:52:45 +0100

    Initial Commit

---
+++ Timekeeper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Timekeeper
+//
+//  Created by Piotr Kłobukowski on 09/01/2020.
+//  Copyright © 2020 Piotr Kłobukowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
